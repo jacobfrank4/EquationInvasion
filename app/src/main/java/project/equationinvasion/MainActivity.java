97a7fcc348914e6c62b4b1d03906b81fd0f7c866
Chun-Yip <koolman987654321@hotmail.com>
2015-05-05 11:49:28 -0700

    THis is me

--- app/src/main/java/project/equationinvasion/MainActivity.java
+++ app/src/main/java/project/equationinvasion/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends ActionBarActivity {
      *
      * HELLLLLO JOOOHNNNN!!!
      *
+     * Chun-Yip WAS HERE!!!
+     *
      * @param savedInstanceState
      */
 
