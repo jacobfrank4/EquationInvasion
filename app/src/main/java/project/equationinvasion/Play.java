6dae1a784ebed4967e6b518a40824ba0072de811
Chun-Yip <koolman987654321@hotmail.com>
2015-05-06 12:04:33 -0700

    Wrong Answer Generator with tentative 10%. Might not work though. Is inside Play.java

--- app/src/main/java/project/equationinvasion/Play.java
+++ app/src/main/java/project/equationinvasion/Play.java
@@ -13,6 +13,7 @@ import java.util.Random;
 
 public class Play extends ActionBarActivity implements View.OnClickListener {
     private TextView equation;
+    private TextView answer;
     Random rand = new Random();
 
     @Override
@@ -23,7 +24,7 @@ public class Play extends ActionBarActivity implements View.OnClickListener {
         final Button newEquation = (Button) findViewById(R.id.button);
         newEquation.setOnClickListener(this);
         equation = (TextView) findViewById(R.id.textView);
-
+        answer = (TextView) findViewById(R.id.textView2);
 
     }
 
@@ -74,21 +75,26 @@ public class Play extends ActionBarActivity implements View.OnClickListener {
     public void add(int a, int b) {
         int correct = a + b;
         askQuestion(a + " + " + b + " = ");
+        askAnswer("" + answerGen(correct));
+
     }
 
     public void subtract(int a, int b) {
         int correct = a - b;
         askQuestion(a + " - " + b + " = ");
+        askAnswer("" + answerGen(correct));
     }
 
     public void divide(int a, int b) {
         int correct = a / b;
         askQuestion(a + " / " + b + " = ");
+        askAnswer("" + answerGen(correct));
     }
 
     public void multiply( int a, int b) {
         int correct = a * b;
         askQuestion(a + " * " + b + " = ");
+        askAnswer("" + answerGen(correct));
     }
 
     private void askQuestion(final String question) {
@@ -104,22 +110,26 @@ public class Play extends ActionBarActivity implements View.OnClickListener {
     public int answerGen(int answer) {
         Random gen = new Random();
 
-        int wrong;
+        double wrong;
         int displayed;
         int choice = gen.nextInt(2) + 1;  // True or False Decision
 
         if (choice == 2) {
-            int variable = gen.nextInt(3) + 1;
+            double variable = 1/10;
             int a = gen.nextInt(2) + 1;
                 if (a == 1) {
                     wrong = answer + variable;
                 } else {
                     wrong = answer - variable;
                 }
-            displayed = wrong;
+            displayed = (int) wrong;
         } else {
             displayed = answer;
         }
         return displayed;
+    }
+
+    private void askAnswer(final String answered) {
+        answer.setText(answered);
     }
 }
