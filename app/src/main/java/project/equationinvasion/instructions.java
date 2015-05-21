8de3252380347dbfa7ee4f661fcc31f5f9da5241
Derek Larson <derek.braden.larson@gmail.com>
2015-05-21 08:47:49 -0700

    Instructions Code clean up and commenting

--- app/src/main/java/project/equationinvasion/instructions.java
+++ app/src/main/java/project/equationinvasion/instructions.java
@@ -1,15 +1,11 @@
 package project.equationinvasion;
 
-import android.app.Activity;
-import android.content.Intent;
+
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
